553e1c5ee91845a3ca8eb7e6fa779a3748977621
ravisdraw <84978160+ravisdraw@users.noreply.github.com>
2022-11-26 20:19:50 +0530

    Add files via upload
    scripting

--- 05 CSS Generator/script.js
+++ 05 CSS Generator/script.js
@@ -1,2 +1,27 @@
-const control = document.getElementById('points');
-console.log(control.value);
+document.addEventListener('DOMContentLoaded', () => {
+    const bar = document.getElementById('points');
+    const txtarea = document.getElementById('txtarea');
+    const val = document.querySelector('.display');
+    const container = document.querySelector('.container');
+
+    var coding = '';
+    var bar_Val = 0;    
+    
+    function updateValues() {
+        bar_Val = bar.value;
+        val.innerText = bar_Val + 'px';
+        coding = `border-radius:${bar_Val}px;`
+        txtarea.value = coding;
+        container.style.cssText = coding;
+    }
+
+    bar.addEventListener('mousemove', updateValues);
+    bar.addEventListener('change', updateValues);
+
+    updateValues();
+});
+
+function copy(){
+    txtarea.select();
+    document.execCommand("copy");
+}
