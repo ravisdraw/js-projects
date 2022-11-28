1da7e64f18ad1c2d12f73eab955439f7a451cb76
ravisdraw <84978160+ravisdraw@users.noreply.github.com>
2022-11-28 08:39:46 +0000

    Add files via upload

--- 05 CSS Generator/script.js
+++ 05 CSS Generator/script.js
@@ -1,27 +1,89 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const bar = document.getElementById('points');
-    const txtarea = document.getElementById('txtarea');
-    const val = document.querySelector('.display');
-    const container = document.querySelector('.container');
+const btnStart = document.querySelector('.start');
+const btnStop = document.querySelector('.stop');
+const btnReset = document.querySelector('.reset');
+const hour = document.querySelectorAll('.hrs');
+const min = document.querySelectorAll('.mins');
+const sec = document.querySelectorAll('.sec');
+const millisec = document.querySelectorAll('.millisec');
 
-    var coding = '';
-    var bar_Val = 0;    
-    
-    function updateValues() {
-        bar_Val = bar.value;
-        val.innerText = bar_Val + 'px';
-        coding = `border-radius:${bar_Val}px;`
-        txtarea.value = coding;
-        container.style.cssText = coding;
-    }
 
-    bar.addEventListener('mousemove', updateValues);
-    bar.addEventListener('change', updateValues);
+let h = m = s = ms = 0;
+let startTimer;
 
-    updateValues();
+btnStart.addEventListener('click', () => {
+    startTimer = setInterval(() => {
+        ms++;
+        if (ms == 100) {
+            ms = 0;
+            s++;
+        }
+        if(s==60){
+            s = 0;
+            m++;
+        }
+        if(m==60){
+            m=0;
+            h++;
+        }
+        updateValues();
+    }, 10);
+    btnStart.classList.remove('active');
+    btnStart.disabled = true;
+    btnStart.classList.add('disable-btn');
+    btnStop.classList.add('deactive');
+    btnStop.disabled = false;
+    btnStop.classList.remove('disable-btn');
+    btnReset.classList.remove('reactive');
+    btnReset.disabled = true;
+    btnReset.classList.add('disable-btn');
 });
 
-function copy(){
-    txtarea.select();
-    document.execCommand("copy");
+btnStop.addEventListener('click', () => {
+    clearInterval(startTimer);
+    btnStop.classList.remove('deactive');
+    btnStart.classList.add('active');
+    btnStart.disabled = false;
+    btnStart.classList.remove('disable-btn');
+    btnReset.classList.add('reactive');
+    btnReset.disabled = false;
+    btnReset.classList.remove('disable-btn');
+});
+
+btnReset.addEventListener('click', () => {
+    h = m = s = ms = 0;
+    updateValues();
+    btnReset.classList.remove('reactive');
+    btnReset.disabled = true;
+    btnReset.classList.add('disable-btn');
+    btnStop.disabled = true;
+    btnStop.classList.add('disable-btn');
+});
+
+
+
+function updateValues() {
+
+    //Format digits
+
+    ph = h < 10 ? '0' + h : h;
+    pm = m < 10 ? '0' + m : m;
+    ps = s < 10 ? '0' + s : s;
+    pms = ms < 10 ? '0' + ms : ms;
+
+    ph = ph.toString();
+    pm = pm.toString();
+    ps = ps.toString();
+    pms = pms.toString();
+
+    hour[0].innerText = ph[0];
+    hour[1].innerText = ph[1];
+
+    min[0].innerText = pm[0];
+    min[1].innerText = pm[1];
+
+    sec[0].innerText = ps[0];
+    sec[1].innerText = ps[1];
+
+    millisec[0].innerText = pms[0];
+    millisec[1].innerText = pms[1];
 }
